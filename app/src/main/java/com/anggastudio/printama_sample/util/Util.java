ec56a0c35e3af63108be751ea2909b546669d93b
Angga Pratama <aa.angga.pratama@gmail.com>
2025-09-10 14:00:06 +0700

    allowing to print

--- app/src/main/java/com/anggastudio/printama_sample/util/Util.java
+++ app/src/main/java/com/anggastudio/printama_sample/util/Util.java
@@ -24,13 +24,14 @@ public class Util {
     }
 
     public static boolean isAllowToPrint() {
-        Integer attempCount = SharedPref.getInt(SharedPref.ATTEMP_COUNT);
-        if (attempCount != null && attempCount < 100) {
-            attempCount++;
-            SharedPref.setInt(SharedPref.ATTEMP_COUNT, attempCount);
-            return true;
-        }
-        return false;
+//        Integer attempCount = SharedPref.getInt(SharedPref.ATTEMP_COUNT);
+//        if (attempCount != null && attempCount < 100) {
+//            attempCount++;
+//            SharedPref.setInt(SharedPref.ATTEMP_COUNT, attempCount);
+//            return true;
+//        }
+//        return false;
+        return true;
     }
 
 }
